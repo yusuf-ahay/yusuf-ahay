d1fbd683e2142ffb936cd88da925cd2348002dd6
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:12:41 +0700

    Create latihan1b.php

---
+++ latihan1b.php
@@ -0,0 +1,18 @@
+<?php
+
+$angka = 10;
+echo "Aku adalah angka $angka. </br>";
+
+$angka *= 8;
+echo "Jika aku di kali 8, jumlahku sekarang $angka. </br>";
+
+$angka /= 4;
+echo "Jika aku di bagi 4, jumlahku sekarang $angka. </br>";
+
+$angka -= 6;
+echo "Jika aku di kurang 6, jumlahku sekarang $angka. </br>";
+
+$angka += 2;
+echo "Jika aku di tambah 2, jumlahku sekarang $angka. </br>";
+
+?>
