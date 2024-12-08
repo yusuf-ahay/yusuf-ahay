7452d0734de729aaa022a898c566175aabbcecbc
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:40:02 +0700

    Create latihan4a.php

---
+++ latihan4a.php
@@ -0,0 +1,11 @@
+<?php
+// Membuat array dengan 4 elemen string
+$warna = array("hijau", "kuning", "kelabu", "merah muda", "biru");
+
+// Menampilkan isi array
+echo "Balonku ada lima.<br>";
+echo "Rupa-rupa warna-nya:<br>";
+echo $warna[0] . ", " . $warna[1] . ", " . $warna[2] . ", " . $warna[3] . ", dan " . $warna[4] . ".<br>";
+echo "Meletus balon " . $warna[0] . " DOR!!!<br>";
+echo "Hatiku sangat kacau.<br>";
+?>
