f1fac88a9aae00b9bcfe14405bd1fb6dd46c8340
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:08:51 +0700

    Create latihan3.php

---
+++ latihan3.php
@@ -0,0 +1,28 @@
+<?php
+
+$x = 19;
+echo "x = $x </br>";
+
+//menggunakan statement IF-ELSE
+if($x %2 ==0){
+    echo "$x adalah bilangan genap";
+} else {
+    echo "$x adalah bilangan ganjil";
+}
+
+echo "</br>";
+
+//menggunakan statement IF-ELSEIF-ELSE
+$tahun = "2018";
+
+if ($tahun == "2015"){
+    echo "sekarang tahun 2015";
+} elseif ($tahun == 2016) {
+    echo "sekarang tahun 2016";
+} elseif ($tahun == 2017) {
+    echo "sekarang tahun 2017";
+} else {
+    echo "sekarang tahun 2018";
+}
+
+?>
