742a48df425e688ccc29bb657b551fe49bb645da
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:10:56 +0700

    Create latihan4.php

---
+++ latihan4.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Looping menggunakan For : </br>";
+for ($i = 1; $i <= 10; $i++) {
+    echo "<li>Urutan ke-$i</li>";
+}
+
+echo "</br>";
+echo "Looping menggunakan While";
+$i = 1;
+while ($i <= 10) {
+    echo "<li>Urutan ke-$i</li>";
+    $i++;
+}
+
+?>
