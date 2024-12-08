a409b6c7e63afd1f76770b2a7ecac60b2f861ef2
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:02:46 +0700

    Create latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "173040888";
+$nama = "lorem ipsum";
+$umur = 20;
+$nilai = 75.75;
+
+echo "nrp : ". $nrp . "</br>";
+echo "nama : $nama</br>";
+print "umur : ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit di belakang koma
+printf ("Nilai  : %.3f</br>", $nilai);
+
+?>
