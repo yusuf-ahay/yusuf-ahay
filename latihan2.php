3cf6843fa7c1a3bf11e78e18354ae9def8200649
yusuf-ahay <ahmatyusufaja@gmail.com>
2024-12-08 13:04:21 +0700

    Create latihan2.php

---
+++ latihan2.php
@@ -0,0 +1,18 @@
+<?php
+
+//menetapkan nilai
+$x = 21;
+$y = 3;
+
+echo "x = $x</br>";
+echo "y = $y</br>";
+
+//pembagian
+$hasilbagi = $x / $y;  //tanda  /  adalah operator
+echo "hasil bagi dari x / y = $hasilbagi </br>";
+
+//perkalian
+$hasilkali =$x * $y; //tanda * adalah operator
+echo "hasil bagi dari x * y =$hasilkali </br>";
+
+?>
